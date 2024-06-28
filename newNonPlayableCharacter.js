f3cb2bef603a92a5732e99203bca1188c3c98d26
Fdavgreen <fdavgreen@gmail.com>
2024-06-28 12:22:00 -0500

    async/await

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,26 +23,34 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+   async function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+       await sleep(time);
+       stop()
     }
 
-    function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
-        element.src = `./assets/red-character/north.gif`
+        element.src = `./assets/red-character/east.gif`
+       await sleep(time);
+       stop()
     }
 
-    function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
-        element.src = `./assets/red-character/west.gif`
+        element.src = `./assets/red-character/east.gif`
+       await sleep(time);
+       stop()
     }
 
-    function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
-        element.src = `./assets/red-character/south.gif`
+        element.src = `./assets/red-character/east.gif`
+       await sleep(time);
+       stop()
     }
-
+    
     function stop() {
         direction = null
         element.src = `./assets/red-character/static.gif`
@@ -57,3 +65,10 @@ function newNonPlayableCharacter(x, y) {
         stop: stop
     }
 }
+
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })  
+}
+
